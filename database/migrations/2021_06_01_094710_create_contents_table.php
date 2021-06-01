eef57ad809a538d2db12d579b65670e837baf3ce
MrNico112 <MrNico112@users.noreply.github.com>
2021-06-01 12:22:35 +0200

    Created category, comment, content models, controllers and migrations. Created columns in the migration files

---
+++ database/migrations/2021_06_01_094710_create_contents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id('contentId')->autoIncrement();
+            $table->string('contentText', 255);
+            $table->text('contentDesc')->nullable();
+            $table->string('contentType', 55);
+            $table->string('contentImage', 255)->nullable();
+            $table->date('contentDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
