204e1e868c88687c3aba3cbb96999297a44e77a4
Nico Kancz <nico.kancz1@gmail.com>
2024-11-14 15:35:33 +0100

    Fix Checkrole issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'restrictRole' => \App\Http\Middleware\CheckRole::class,
+        'restrictRole' => \app\Http\Middleware\CheckRole::class,
     ];
 }
--- app/Http/Middleware/checkRole.php
+++ app/Http/Middleware/checkRole.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class checkRole
+class CheckRole
 {
     /**
      * Handle an incoming request.
